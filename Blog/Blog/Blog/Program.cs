c1441553ad6c5f14a384465d1d71c55c3a7ea307
André Morsch <andremorsch37@gmail.com>
2022-11-29 17:45:31 -0300

    Aulas

---
+++ Blog/Blog/Blog/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Data.SqlClient;
+
+const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$";
+
+var connection = new SqlConnection(CONNECTION_STRING);
+connection.Open();
+
+connection.Close();
+Console.ReadKey();
---
+++ Blog/Blog/Blog/Screens/TagScreens/MenuTagScreen.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Screens.TagScreens
+{
+    public static class MenuTagScreen
+    {
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("Tags");
+            Console.WriteLine("---------------");
+            Console.WriteLine("1 - List tags");
+            Console.WriteLine("2 - Create tag");
+            Console.WriteLine("3 - Update tag");
+            Console.WriteLine("4 - Delete tag");
+            Console.WriteLine("5 - Back");
+            Console.WriteLine("---------------");
+            Console.Write("Choose an option: ");
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    ListTagScreen.Load();
+                    break;
+                case "2":
+                    CreateTagScreen.Load();
+                    break;
+                case "3":
+                    UpdateTagScreen.Load();
+                    break;
+                case "4":
+                    DeleteTagScreen.Load();
+                    break;
+                case "5":
+                    Program.Load();
+                    break;
+                default:
+                    Load();
+                    break;
+            }
+        }
+    }
+}
